d2eb2021f1eff2593318afb388576bfe9d35380a
xj-hua <xijinhua@outlook.com>
2021-06-29 22:54:21 +0100

    Main code_batch simulation of drop landing and plot of results

--- AFO_Simulation_Main.py
+++ AFO_Simulation_Main.py
@@ -7,8 +7,8 @@ import numpy as np
 import AFO4_ResultsCollection
 import AFO5_DoE
 import pandas as pd
-import plotly
-import plotly.graph_objs as go
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Batch simulation for the drop landing
@@ -26,12 +26,12 @@ FL_shift_side_Vardeci=2**FL_shift_side_Varbin-1
 FL_shift_front_Vardeci=2**FL_shift_front_Varbin-1
 """
 """
-for asi in range (0,10):                                        # The number of the amplification variables for side FL relationship
-    for afj in range (0,10):                                    # The number of the amplification variables for front FL relationship
+for asi in range (4,13):                                        # The number of the amplification variables for side FL relationship
+    for afj in range (4,13):                                    # The number of the amplification variables for front FL relationship
         for ssm in range (0,10):                              # The number of the shift variables for side FL relationship
             for sfn in range (0,10):                           # The number of the shift variables for front FL relationship
-                FL_amplification_side_Vardeci=asi*10+1
-                FL_amplification_front_Vardeci=afj*10+1
+                FL_amplification_side_Vardeci=asi*10
+                FL_amplification_front_Vardeci=afj*10
                 FL_shift_side_Vardeci=ssm*0.04-0.2
                 FL_shift_front_Vardeci=sfn*0.04-0.2
                 # Change the AFO material properties in input file
@@ -40,50 +40,70 @@ for asi in range (0,10):                                        # The number of 
                 AFO0_Simulation.Simulation('AFODroplanding', 'simulation', ResultDirectory)
                 # Restore the AFO material properties in input file to the baseline materials
                 AFO3_ParaTestSelect.AFOmaterialVariables(1/FL_amplification_side_Vardeci, -FL_shift_side_Vardeci, 1/FL_amplification_front_Vardeci, -FL_shift_front_Vardeci)
-                print(ResultDirectory)
-"""
-"""
+                # print(ResultDirectory)
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# Put the simulation results from the results folders to an excel file
+"""
+
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#  Put the simulation results from the results folders to an excel documents
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+"""
 Results_parameter=['time', '/jointset/subtalar_r/subtalar_angle_r/value', '/jointset/ankle_r/ankle_angle_r/value']                                          # The specified parameter to extract
 Subtalar_matrix=[]
-for asi in range (0,9):                                        # The number of the amplification variables for side FL relationship
-    for afj in range (0,10):                                    # The number of the amplification variables for front FL relationship
+for asi in range (4,13):                                        # The number of the amplification variables for side FL relationship
+    for afj in range (4,13):                                    # The number of the amplification variables for front FL relationship
         for ssm in range (0,10):                              # The number of the shift variables for side FL relationship
             for sfn in range (0,10):                           # The number of the shift variables for front FL relationship
-                output_folder=ResultDirectory='Drop landing\SimulationOutput_DL_'+str(asi)+str(afj)+str(ssm)+str(sfn)                # The folder of the FD results
+                output_folder=ResultDirectory='Drop landing_platform25\SimulationOutput_DL_'+str(asi)+str(afj)+str(ssm)+str(sfn)                # The folder of the FD results
                 data= AFO4_ResultsCollection.Simulationresultscollection(output_folder, Results_parameter, 'default_states_degrees.mot')                        # put the specified results into a matrix
                 if data.size==0:                                # If the folder does not exit, skip the current loop to the next one.
                     continue
                 else:
-                    Subtalar_matrix.append([asi*10+1, afj*10+1, ssm*0.04-0.2, sfn*0.04-0.2, max(data[:,1])])           # Put the four variables and the subtalar angles into a list
+                    Subtalar_matrix.append([asi*10, afj*10, ssm*0.04-0.2, sfn*0.04-0.2, max(data[:,1])])           # Put the four variables and the subtalar angles into a list
 Subtalar_matrix=np.array(Subtalar_matrix)                                                        # Transform the list to the matrix
 Excel_title=['Side_amplification','Front_amplification','Side_shift','Front_shift','Max subtalar']              # Define the title of the excel
-AFO4_ResultsCollection.DLResultstoExcel('Drop landing', 'DL Results.xls', 'Platform 30', Excel_title, Subtalar_matrix)          # Put the four variables and subtalar angles to an excel
+AFO4_ResultsCollection.DLResultstoExcel('Drop landing_platform25', 'DL Results.xls', 'Platform 25', Excel_title, Subtalar_matrix)          # Put the four variables and subtalar angles to an excel
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 """
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# Draw 5D figures for the 4 variables
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-data=AFO4_ResultsCollection.ReadExcel('Drop landing', 'DL Results.xls','Platform 30')
-fig1=go.Scatter3d(x=data['Side_amplification'],
-                               y=data['Side_shift'],
-                               z=data['Front_shift'],
-                               marker=dict(size=data['Front_amplification']/8,
-                                                    color=data['Max subtalar'],
-                                                    opacity=0.9,
-                                                    reversescale=True,
-                                                    colorscale='rainbow'),
-                                line=dict(width=0.02),
-                                mode='markers')
-mylayout=go.Layout(scene=dict(xaxis=dict(title="Side_amplification"),
-                                                    yaxis=dict(title="Front_amplification"),
-                                                    zaxis=dict(title="Side_shift")),)
 
-plotly.offline.plot({"data": [fig1],
-                                "layout": mylayout},
-                                 auto_open=True,
-                                 filename=("AFO properties.html"))
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#  Put the results in a matrix from an excel file and plot the results in a 4D scatter figures
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Put the results from an excel file into a matrix
+data=AFO4_ResultsCollection.ReadExcel('Drop landing_platform25', 'DL Results.xls','Platform 25')
+data_smallthan30=data[data['Max subtalar']<30].values                          # Matix including subtalar angles smaller than 30 degree
+data_bigthan30=data[data['Max subtalar']>=30].values                           # Matix including subtalar angles bigger than 30 degree
+# Plot 4D scrater figures for the results
+fig=plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x1=data_smallthan30[:,0]
+x2=data_bigthan30[:,0]
+y1=data_smallthan30[:,1]
+y2=data_bigthan30[:,1]
+z1=data_smallthan30[:,2]
+z2=data_bigthan30[:,2]
+z11=data_smallthan30[:,3]
+z22=data_bigthan30[:,3]
+c1=data_smallthan30[:,4]
+c2=data_bigthan30[:,4]
+#img=ax.scatter(x2,y2,z22,s=100,c=c2,cmap=plt.autumn())
+img=ax.scatter(x1,y1,z11,s=100,c=c1,cmap=plt.summer())
+# Choice available: hot, cool, gray, bone, white, spring, summer, autumn, winter
+ax.set_xlabel("Side_amplifcation")
+ax.set_ylabel("Front_amplification")
+ax.set_zlabel("Front_shift")
+fig.colorbar(img)
+
+plt.show()
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+
+
+
 
 
 
@@ -160,8 +180,6 @@ print(s)
 # Drop landing DoE
 #  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 """
-
-
 
 
 # Previous old code
