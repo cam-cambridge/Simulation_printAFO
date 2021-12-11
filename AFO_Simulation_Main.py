7a0140a84ec9878ff42c3ae5b5928ceddf9e52cc
xj-hua <xijinhua@outlook.com>
2021-12-11 00:01:08 +0000

    The main code for the batch simulation of drop landing, walk and running, calling all the modules

--- AFO_Simulation_Main.py
+++ AFO_Simulation_Main.py
@@ -13,11 +13,13 @@ import itertools
 import tkinter
 from tkinter import filedialog
 
-"""
+#AFO0_Simulation.Simulation('walk', 'simulation', results_directory='')
+#AFO0_Simulation.Simulation('run', 'simulation', results_directory='')
+
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # (1) Batch simulation for the DL, walk and run
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-for fl_am_1, fl_am_2, fl_am_3, fl_am_4 in itertools.product(range(3,4), range(8,9), range(1,2), range(3,4)):                                                                                        # Design parameter: force-length amplification (fl_am)
+for fl_am_1, fl_am_2, fl_am_3, fl_am_4 in itertools.product(range(2,4), range(8,9), range(1,2), range(3,4)):                                                                                        # Design parameter: force-length amplification (fl_am)
     for fl_shift_1, fl_shift_2, fl_shift_3, fl_shift_4 in itertools.product(range(1,2), range(0,1), range(2,3), range(2,3)):                                                                           # Design parameter: force-length shift (fl_shift)
         for strip_ori_1, strip_ori_2, strip_ori_3, strip_ori_4 in itertools.product(range(0,1), range(1,2), range(0,1), range(0,1)):                                                               # Deisgn parameter: stripe orientation (strip_ori)
             for  bottom_location_1, bottom_location_2, bottom_location_3, bottom_location_4 in itertools.product(range(0,1), range(0,1), range (0,1), range(0,1)):      # Design parameter: bottom endpoint location (bottom_location)
@@ -70,7 +72,6 @@ for fl_am_1, fl_am_2, fl_am_3, fl_am_4 in itertools.product(range(3,4), range(8,
                 # 1/FL_amplification_1, -FL_shift_1: invalid parameters, 'Resume design file': command for resuming the design parameter txt file
                 AFO3_ParaTestSelect.AFOmaterialVariables(1/FL_amplification_1, -FL_shift_1, 'Resume design file')
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-"""
 
 """
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
