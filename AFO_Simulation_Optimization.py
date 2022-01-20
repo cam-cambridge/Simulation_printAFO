559696a8ae7fb2dcc6b2e3c36aa91458a8f6cb32
xj-hua <xijinhua@outlook.com>
2022-01-20 23:15:24 +0000

    The module of simulation of drop landing, walk and running used for the Gradient Descent optimization, calling all the modules

--- AFO_Simulation_Optimization.py
+++ AFO_Simulation_Optimization.py
@@ -13,25 +13,35 @@ import itertools
 import tkinter
 from tkinter import filedialog
 import math
+import multiprocessing
+from multiprocessing import Pool
 
-def Main_Simulation (DesignVariables):
+def Main_Simulation (DesignVariables, folder_index):
     #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     # Simulations of drop landing, walk and Running
+    """
     # The drop landing simulation DL
-    AFO0_Simulation.Simulation('AFODroplanding', 'simulation', DesignVariables, 'SimulationOutput_DL_AFO')
+    #AFO0_Simulation.Simulation('AFODroplanding', 'simulation', DesignVariables, 'SimulationOutput_DL_AFO'+str(folder_index))
+    AFO0_Simulation.Simulation('AFODroplanding', 'simulation', DesignVariables, str(folder_index))
     # The walking simulation Walk
-    AFO0_Simulation.Simulation('Walk_AFO', 'simulation', DesignVariables, 'SimulationOutput_Walk_AFO')
+    #AFO0_Simulation.Simulation('Walk_AFO', 'simulation', DesignVariables, 'SimulationOutput_Walk_AFO'+str(folder_index))
+    AFO0_Simulation.Simulation('Walk_AFO', 'simulation', DesignVariables, str(folder_index))
     # The running simulation Run
-    AFO0_Simulation.Simulation('Run_AFO', 'simulation', DesignVariables, 'SimulationOutput_Run_AFO')
+    #AFO0_Simulation.Simulation('Run_AFO', 'simulation', DesignVariables, 'SimulationOutput_Run_AFO'+str(folder_index))
+    AFO0_Simulation.Simulation('Run_AFO', 'simulation', DesignVariables, str(folder_index))
+    """
+    # The parallel simulation of the drop landing, walk and running
+    Parallel_simu_paralist=[('AFODroplanding', 'simulation', DesignVariables, str(folder_index)), ('Walk_AFO', 'simulation', DesignVariables, str(folder_index)), ('Run_AFO', 'simulation', DesignVariables, str(folder_index))]
+    pool=multiprocessing.Pool()
+    pool.map(AFO0_Simulation.Simulation, Parallel_simu_paralist)
     #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     # Collect the simulation results for drop landing, walk and running
     #-----------------------------------------------------------------------------
     # For drop landing activity, collect the maximum subtalar angle and ankle angle
     Results_parameter_DL=['time', '/jointset/subtalar_r/subtalar_angle_r/value', '/jointset/ankle_r/ankle_angle_r/value']                                          # The specified parameter to extract
-    output_folder_DL='Drop landing\DL simulation results\SimulationOutput_DL_AFO'
+    output_folder_DL='Simulation models\Drop landing'+str(folder_index)+'\DL simulation results\\'+str(folder_index)
     data_DL= AFO4_ResultsCollection.Simulationresultscollection(output_folder_DL, Results_parameter_DL, 'default_states_degrees.mot')      # put the specified results into a matrix
     Subtalar_DL_max=max(data_DL[:,1])                                                                                                                                                                    # The maximum subtalar angle during drop landing
-
     #-----------------------------------------------------------------------------
     #  For walk, collect the average differences of muscle forces between the models with and without AFO cross the whole cyecle
     #  inteplote the curve across the whole cycle into lots of points (equal to the time instances for model without AFO) and calculate the differences of muscle forces for these points
@@ -40,11 +50,11 @@ def Main_Simulation (DesignVariables):
                                       'gasmed_r', 'glmax1_r', 'glmax2_r', 'glmax3_r', 'glmed1_r', 'glmed2_r', 'glmed3_r', 'glmin1_r', 'glmin2_r', 'glmin3_r', 'grac_r', 'iliacus_r', 'perbrev_r',
                                       'perlong_r', 'piri_r', 'psoas_r', 'recfem_r', 'sart_r', 'semimem_r', 'semiten_r', 'soleus_r', 'tfl_r', 'tibant_r', 'tibpost_r', 'vasint_r', 'vaslat_r', 'vasmed_r']
     # The folder that includes the results of walking simulation without AFO
-    output_folder_walk_withoutAFO='Gait simulation\Model outputs\\4_CMC\SimulationOutput_walk_withoutAFO'
+    output_folder_walk_withoutAFO='Simulation models\Gait simulation'+str(folder_index)+'\Model outputs\\4_CMC\SimulationOutput_walk_withoutAFO'
     # The total muscle force for all the muscles in the right leg
     data_walk_withoutAFO=AFO4_ResultsCollection.Simulationresultscollection(output_folder_walk_withoutAFO, Results_parameter_walk, 'cmc_MuscleAnalysis_TendonForce.sto')
     # The folder that includes the results of walking simulation with AFO
-    output_folder_walk_AFO='Gait simulation\Model outputs\\4_CMC\SimulationOutput_walk_AFO'
+    output_folder_walk_AFO='Simulation models\Gait simulation'+str(folder_index)+'\Model outputs\\4_CMC\\'+str(folder_index)
     # The total muscle force for all the muscles in the right leg
     data_walk_AFO=AFO4_ResultsCollection.Simulationresultscollection(output_folder_walk_AFO, Results_parameter_walk, 'cmc_MuscleAnalysis_TendonForce.sto')
     diff_average_musforce_WholeMuscle_walk=[]
@@ -52,7 +62,6 @@ def Main_Simulation (DesignVariables):
         [diff_total_musforce_walk, diff_average_musforce_walk, diff_max_musforce_walk]=AFO4_ResultsCollection.curvecomparison(data_walk_withoutAFO, data_walk_AFO, muscle_num, len(data_walk_withoutAFO))
         diff_average_musforce_WholeMuscle_walk.append(diff_average_musforce_walk)                                # The matrix include the absolute differences of muscle forces for each musch for each design case
         diff_average_musforce_total_walk=np.sum(diff_average_musforce_WholeMuscle_walk)                     # The total differences of muscle forces for all the muscle in the right legs for each design case
-
     #-----------------------------------------------------------------------------
     #  For running, collect the average differences of muscle forces between the models with and without AFO cross the whole cyecle
     #  inteplote the curve across the whole cycle into lots of points (equal to the time instances for model without AFO) and calculate the differences of muscle forces for these points
@@ -61,11 +70,11 @@ def Main_Simulation (DesignVariables):
                                       'gasmed_r', 'glmax1_r', 'glmax2_r', 'glmax3_r', 'glmed1_r', 'glmed2_r', 'glmed3_r', 'glmin1_r', 'glmin2_r', 'glmin3_r', 'grac_r', 'iliacus_r', 'perbrev_r',
                                       'perlong_r', 'piri_r', 'psoas_r', 'recfem_r', 'sart_r', 'semimem_r', 'semiten_r', 'soleus_r', 'tfl_r', 'tibant_r', 'tibpost_r', 'vasint_r', 'vaslat_r', 'vasmed_r']
     # The folder that includes the results of running simulation without AFO
-    output_folder_run_withoutAFO='Running simulation\Model outputs\\4_CMC\SimulationOutput_Run_withoutAFO'
+    output_folder_run_withoutAFO='Simulation models\Running simulation'+str(folder_index)+'\Model outputs\\4_CMC\SimulationOutput_Run_withoutAFO'
     # The total muscle force for all the muscles in the right leg
     data_run_withoutAFO=AFO4_ResultsCollection.Simulationresultscollection(output_folder_run_withoutAFO, Results_parameter_run, 'cmc_MuscleAnalysis_TendonForce.sto')
     # The folder that includes the results of running simulation with AFO
-    output_folder_run_AFO='Running simulation\Model outputs\\4_CMC\SimulationOutput_Run_AFO'
+    output_folder_run_AFO='Simulation models\Running simulation'+str(folder_index)+'\Model outputs\\4_CMC\\'+str(folder_index)
     # The total muscle force for all the muscles in the right leg
     data_run_AFO=AFO4_ResultsCollection.Simulationresultscollection(output_folder_run_AFO, Results_parameter_run, 'cmc_MuscleAnalysis_TendonForce.sto')
     diff_average_musforce_WholeMuscle_run=[]
